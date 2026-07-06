cd22a538c944759f64b654bd94a36491d2ae065b
RashikShahjahan <rashikshahjahan@protonmail.com>
2026-07-05 22:02:06 -0400

    in

--- index.ts
+++ index.ts
@@ -3,9 +3,6 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 const port = 3000;
 
-app.get('/health', (req: Request, res: Response) => {
-  res.send(200);
-});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
